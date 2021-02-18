3ba27e4de3a61b68382e828c22562e155dd3fdf2
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-02-18 10:50:03 -0300

    🚅perfs: melhorando definição de funções para modo elixir

--- lib/poc_cade_esse_cep_web/utils/validates.ex
+++ lib/poc_cade_esse_cep_web/utils/validates.ex
@@ -1,11 +1,6 @@
 defmodule PocCadeEsseCepWeb.Validades do
   @moduledoc "Um modulo de validação"
 
-  def validate_size_zip_code(zip) do
-    if String.length(zip) == 8 do
-      zip
-    else
-      {:error, "Não é válido"}
-    end
-  end
+  def validate_size_zip_code(zip) when zip |> length == 8, do: zip
+  def validate_size_zip_code(_zip), do: {:error, "Não é válido"}
 end
